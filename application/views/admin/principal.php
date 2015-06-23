9acbf21ee247be0572a437860a8e9ea030d75c4f
Chomo <iscalmeda@gmail.com>
2015-06-23 14:39:04 -0500

    Las tabs funcionana correctamente

--- application/views/admin/principal.php
+++ application/views/admin/principal.php
@@ -47,60 +47,55 @@ $buscar = array(
 ?>
 
 
-<div class="row">
-	<div class="col-sm-12">
-		 <ul class="nav nav-tabs" role="tablist">
-		    <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Agenda</a></li>
-		    <li role="presentation"><a href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Documentos</a></li>
-		 </ul>
 
-		  <!-- Tab panes -->
-		  <div class="tab-content">
-		    <div role="tabpanel" class="tab-pane active" id="home">
-		    		<div class="formulario" >
-						<?=form_open('Admin/captura')?>
-						<table class="table table-bordered">
-							<tr>
-								<th colspan="2">
-									<center>Agregar Evento</center>
-								</th>
-							</tr>
-							<tr>
-								<th>Nombre:</th>
-								<td><?=form_input($evento)?></td>
-							</tr>
-							<tr>
-								<th>Lugar:</th>
-								<td><?=form_input($lugar)?></td>
-							</tr>
-							<tr>
-								<th>Fecha:</th>
-								<td><?=form_input($fecha)?></td>
-							</tr>
-							<tr>
-								<th>Hora:</th>
-								<td><?=form_input($hora)?></td>
-							</tr>
-							<tr>
-								<th colspan='2'>
-									<center><?=form_input($boton)?></center>
-								</th>
-							</tr>
-						</table>
-						<?=form_close()?>
-					</div>
-					<div class="col-sm-12">
-						<?=form_open('Admin/buscar')?>
-						<table class="table table-bordered">
-							<tr>
-								<th colspan='2'><?=form_input($buscar)?></th><td><center><?=form_input($botonB)?></center></td>
-							</tr>
-						</table>
-						<?=form_close()?>
-					</div>
-			</div>
-				
-		    <div class="col-sm-12">
+    <ul id="myTabs" class="nav nav-tabs" role="tablist">
+      <li role="presentation" class="active"><a href="#home" id="home-tab" role="tab" data-toggle="tab" aria-controls="home" aria-expanded="true">Agenda</a></li>
+      <li class="" role="presentation"><a aria-expanded="false" href="#profile" role="tab" id="profile-tab" data-toggle="tab" aria-controls="profile">Documentos</a></li>
+    </ul>
+    <div id="myTabContent" class="tab-content">
+      <div role="tabpanel" class="tab-pane fade active in" id="home" aria-labelledby="home-tab">
+        <div class="formulario" >
+			<?=form_open('Admin/captura')?>
+				<table class="table table-bordered">
+					<tr>
+						<th colspan="2">
+							<center>Agregar Evento</center>
+						</th>
+					</tr>
+					<tr>
+						<th>Nombre:</th>
+						<td><?=form_input($evento)?></td>
+					</tr>
+					<tr>
+						<th>Lugar:</th>
+							<td><?=form_input($lugar)?></td>
+					</tr>
+					<tr>
+						<th>Fecha:</th>
+						<td><?=form_input($fecha)?></td>
+					</tr>
+					<tr>
+						<th>Hora:</th>
+						<td><?=form_input($hora)?></td>
+					</tr>
+					<tr>
+					<th colspan='2'>
+							<center><?=form_input($boton)?></center>
+						</th>
+					</tr>
+				</table>
+			<?=form_close()?>
+		</div>
+		<div class="col-sm-12">
+			<?=form_open('Admin/buscar')?>
+				<table class="table table-bordered">
+					<tr>
+						<th colspan='2'><?=form_input($buscar)?></th><td><center><?=form_input($botonB)?></center></td>
+					</tr>
+				</table>
+			<?=form_close()?>
+		</div>
+		<div class="col-sm-12">
 					<table class="table table-bordered">
 						<tr>
 							<th colspan="4">
@@ -109,48 +104,52 @@ $buscar = array(
 								</center>
 							</th>
 						</tr>
-							<? 
-								if($agenda->num_rows() > 0){
-									echo "<tr><th>Fecha</th><th>Evento</th><th>Hora</th><th>Lugar</th></tr>";
-									foreach($agenda->result() as $row){
-										echo "<tr><td>".$row->Fecha."</td><td>".$row->Evento."</td><td>".$row->Hora."</td><td>".$row->Lugar."</td></tr>";
-									}
-								}else{
-									echo "<tr><th colspan='4'><center>No hay Ningun Evento para el dia de hoy</center></th></tr>";
-								}
-							?>
+				<? 
+				if($agenda->num_rows() > 0){
+				echo "<tr><th>Fecha</th><th>Evento</th><th>Hora</th><th>Lugar</th></tr>";
+					foreach($agenda->result() as $row){
+						echo "<tr><td>".$row->Fecha."</td><td>".$row->Evento."</td><td>".$row->Hora."</td><td>".$row->Lugar."</td></tr>";
+					}
+				}else{
+					echo "<tr><th colspan='4'><center>No hay Ningun Evento para el dia de hoy</center></th></tr>";
+				}
+				?>
+			</table>
+		</div>
+		<div class="row">
+			<div class="col-sm-12">
+				<table class="table table-bordered">
+					<tr>
+						<th colspan="4">
+							<center>
+								<h1>Agenda de Mañana</h1>
+							</center>
+						</th>
+					</tr>
+					<? 
+					if($manana->num_rows() > 0){
+						echo "<tr><th>Fecha</th><th>Evento</th><th>Hora</th><th>Lugar</th></tr>";
+							foreach($agenda->result() as $row){
+								echo "<tr><td>".$row->Fecha."</td><td>".$row->Evento."</td><td>".$row->Hora."</td><td>".$row->Lugar."</td></tr>";
+							}
+					}else{
+						echo "<tr><th colspan='4'><center>No hay Ningun Evento para el dia de hoy</center></th></tr>";
+					}
+					?>
 					</table>
-			</div>
-			<div class="row">
-					<div class="col-sm-12">
-						<table class="table table-bordered">
-							<tr>
-								<th colspan="4">
-									<center>
-										<h1>Agenda de Mañana</h1>
-									</center>
-								</th>
-							</tr>
-								<? 
-									if($manana->num_rows() > 0){
-										echo "<tr><th>Fecha</th><th>Evento</th><th>Hora</th><th>Lugar</th></tr>";
-										foreach($agenda->result() as $row){
-											echo "<tr><td>".$row->Fecha."</td><td>".$row->Evento."</td><td>".$row->Hora."</td><td>".$row->Lugar."</td></tr>";
-										}
-									}else{
-										echo "<tr><th colspan='4'><center>No hay Ningun Evento para el dia de hoy</center></th></tr>";
-									}
-								?>
-						</table>
-					</div>
-			</div>
-		  </div>
-		  <div class="tab-content">
-			  <div role="tabpanel" class="tab-pane" id="profile">
-			    	<center>	
-			    		Otro doc
-			    	</center>	
-			  </div>
-		  </div>
-	</div>
-</div>
+				</div>
+		</div>
+
+
+
+    </div>
+
+
+
+      <div role="tabpanel" class="tab-pane fade" id="profile" aria-labelledby="profile-tab">
+        Otro Doc
+      </div>
+    </div>
+  
+
+
